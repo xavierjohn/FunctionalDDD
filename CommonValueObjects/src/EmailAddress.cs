f4fcade1ab0cfd3d62a21f54dad9fb8cb0c804e1
Xavier <xavierjohn@hotmail.com>
2022-12-05 15:51:11 -0800

    Email address ValueObject (#3)

---
+++ CommonValueObjects/src/EmailAddress.cs
@@ -0,0 +1,20 @@
+﻿namespace FunctionalDDD.CommonValueObjects;
+
+using System.Text.RegularExpressions;
+using FunctionalDDD.Core;
+
+public partial class EmailAddress : SimpleValueObject<string>
+{
+    private EmailAddress(string value) : base(value) { }
+
+    public static Result<EmailAddress> Create(string emailString, string? fieldName = null)
+    {
+        var isEmail = EmailRegEx().IsMatch(emailString);
+        if (isEmail) return new EmailAddress(emailString);
+
+        return Result.Failure<EmailAddress>(Error.Validation(fieldName ?? "email", "Email is not valid"));
+    }
+
+    [GeneratedRegex("\\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\\Z", RegexOptions.IgnoreCase, "en-US")]
+    private static partial Regex EmailRegEx();
+}
---
+++ CommonValueObjects/tests/EmailAddressTests.cs
@@ -0,0 +1,39 @@
+using FunctionalDDD.CommonValueObjects;
+using FunctionalDDD.Core;
+
+namespace CommonValueObjects.Tests;
+
+public class EmailAddressTests
+{
+    [Theory]
+    [InlineData("xavier")]
+    [InlineData("xavier@")]
+    [InlineData("")]
+    [InlineData("@com")]
+    [InlineData("@#@@##@%^%#$@#$@#.com")]
+    [InlineData("John Doe <example@email.com>")]
+    [InlineData("CAT…123@email.com")]
+    public void Cannot_create_invalid_email(string email)
+    {
+        // Arrange
+        var result = EmailAddress.Create(email, "school email");
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(Error.Validation("school email", "Email is not valid"));
+    }
+
+    [Theory]
+    [InlineData("xavier@somewhere.com")]
+    [InlineData("0987654321@example.com")]
+    [InlineData("_______@email.com")]
+    public void Can_create_valid_email(string email)
+    {
+        // Arrange
+        var result = EmailAddress.Create(email, "school email");
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeOfType<EmailAddress>();
+    }
+}
