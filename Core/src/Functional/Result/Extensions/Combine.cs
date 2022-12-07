f52a4b836bc87e2003762239526f54af50bfb768
Xavier John <xavierjohn@hotmail.com>
2022-12-07 10:02:20 -0800

    Not working.

--- Core/src/Functional/Result/Extensions/Combine.cs
+++ Core/src/Functional/Result/Extensions/Combine.cs
@@ -16,10 +16,9 @@ public static partial class ResultExtensions
 
         return Result.Success((t1.Value, t2.Value));
     }
-    
-    public static Result<(T1, T2)> Combine<T1, T2>(this Result<T1> t1, Func<Result<T2>> ft2)
+
+    public static Result<(T1, T2, T3)> Combine<T1, T2, T3>(this Result<(T1, T2)> t1, Result<T3> t2)
     {
-        Result<T2> t2 = ft2();
         if (t1.IsFailure || t2.IsFailure)
         {
             var errors = new ErrorList();
@@ -27,9 +26,25 @@ public static partial class ResultExtensions
                 errors.AddRange(t1.Errors);
             if (t2.IsFailure)
                 errors.AddRange(t2.Errors);
-            return Result.Failure<(T1, T2)>(errors);
+            return Result.Failure<(T1, T2, T3)>(errors);
         }
 
-        return Result.Success((t1.Value, t2.Value));
+        return Result.Success((t1.Value.Item1, t1.Value.Item2, t2.Value));
     }
+
+    public static Result<(T1, T2, T3, T4)> Combine<T1, T2, T3, T4>(this Result<(T1, T2, T3)> t1, Result<T4> t2)
+    {
+        if (t1.IsFailure || t2.IsFailure)
+        {
+            var errors = new ErrorList();
+            if (t1.IsFailure)
+                errors.AddRange(t1.Errors);
+            if (t2.IsFailure)
+                errors.AddRange(t2.Errors);
+            return Result.Failure<(T1, T2, T3, T4)>(errors);
+        }
+        
+        return Result.Success((t1.Value.Item1, t1.Value.Item2, t1.Value.Item3, t2.Value));
+    }
+
 }
--- Example/ValidationExample.cs
+++ Example/ValidationExample.cs
@@ -8,15 +8,16 @@ public class ValidationExample
     [Fact]
     public void Test1()
     {
+
         var x = EmailAddress.Create("xavier@somewhere.com")
             .Combine(FirstName.Create("Xavier"))
-            .Combine(LastName.Create("John"));
-//            .Validate(LastName.Create(string.empty));
-        
-//.AndValidate(FirstName.Create(string.empty))
-//.Bind((email, lastName, firstName) => CreateUserCommand.Create(firstName, lastName, email))
-//.Tap(command => m.Send(command))
-//.Finally(r => MapToActionResult(r));
+            .Combine(LastName.Create("John"))
+            .Combine(EmailAddress.Create("xavier@somewhereelse.com"));
 
+        string combinedString;
+        x.Bind((email, lastName, firstName, anotherEmail) => combinedString = firstName + lastName + email + anotherEmail);
+
+        //Error	CS1929	'Result<(EmailAddress, FirstName, LastName, EmailAddress)>' does not contain a definition for 'Bind'
+        //and the best extension method overload 'ResultExtensions.Bind(UnitResult, Func<UnitResult>)' requires a receiver of type 'UnitResult'
     }
 }
