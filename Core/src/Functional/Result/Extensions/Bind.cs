b0bcb4906a0f3e8ccd81db9d0d0c80df60a81cea
Xavier John <xavierjohn@hotmail.com>
2022-12-07 14:55:39 -0800

    Combine works

--- Core/src/Functional/Result/Extensions/Bind.cs
+++ Core/src/Functional/Result/Extensions/Bind.cs
@@ -5,11 +5,25 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
     /// </summary>
-    public static Result<K> Bind<T, K>(this Result<T> result, Func<T, Result<K>> func)
+    public static Result<TResult> Bind<T, TResult>(this Result<T> result, Func<T, Result<TResult>> func)
     {
         if (result.IsFailure)
-            return Result.Failure<K>(result.Error);
+            return Result.Failure<TResult>(result.Errors);
 
         return func(result.Value);
+    }
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2, T3, T4, TResult>(
+        this Result<(T1, T2, T3, T4)> result
+        , Func<T1, T2, T3, T4, Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (arg1, arg2, arg3, arg4) = result.Value;
+        return func(arg1, arg2, arg3, arg4);
     }
 }
--- Example/ValidationExample.cs
+++ Example/ValidationExample.cs
@@ -1,16 +1,13 @@
-using System;
-using System.ComponentModel;
 using FluentAssertions;
 using FunctionalDDD.CommonValueObjects;
 using FunctionalDDD.Core;
-using Newtonsoft.Json;
 
 namespace Example;
 
 public class ValidationExample
 {
     [Fact]
-    public void Test1()
+    public void Validation_successful_Test()
     {
 
         var x = EmailAddress.Create("xavier@somewhere.com")
@@ -18,16 +15,30 @@ public class ValidationExample
             .Combine(LastName.Create("John"))
             .Combine(EmailAddress.Create("xavier@somewhereelse.com"));
 
-        var actual = x.Bind(x => Result.Success(string.Join(" ", x.Item2, x.Item3, x.Item1, x.Item4)));
-
-        // var actual = x.Bind((email, firstName, lastName, anotherEmail) => Result.Success(firstName + lastName + email + anotherEmail));
-        // Error CS0411  The type arguments for method 'ResultExtensions.Bind<T, K>(Result<T>, Func<T, Result<K>>)' cannot be inferred from the usage.Try specifying the type arguments explicitly.
-
-
-        //var actual = x.Bind<Result<(EmailAddress, FirstName, LastName, EmailAddress)>, string>((email, firstName, lastName, anotherEmail) => Result.Success(firstName + lastName + email + anotherEmail));
-        // Error CS1929  'Result<(EmailAddress, FirstName, LastName, EmailAddress)>' does not contain a definition for 'Bind' and the best extension method overload 'ResultExtensions.Bind<Result<(EmailAddress, FirstName, LastName, EmailAddress)>, string>(Result<Result<(EmailAddress, FirstName, LastName, EmailAddress)>>, Func<Result<(EmailAddress, FirstName, LastName, EmailAddress)>, Result<string>>)' requires a receiver of type 'Result<Result<(EmailAddress, FirstName, LastName, EmailAddress)>>'
-
-
+        var actual = x.Bind((email, firstName, lastName, anotherEmail) => Result.Success(string.Join(" ", firstName, lastName, email, anotherEmail)));
         actual.Value.Should().Be("Xavier John xavier@somewhere.com xavier@somewhereelse.com");
     }
+
+    [Fact]
+    public void Validation_failed_Test()
+    {
+
+        var x = EmailAddress.Create("xavier@somewhere.com")
+            .Combine(FirstName.Create("Xavier"))
+            .Combine(LastName.Create(string.Empty))
+            .Combine(EmailAddress.Create("xavier @ somewhereelse.com"));
+
+        var actual = x.Bind((email, firstName, lastName, anotherEmail) =>
+        {
+            true.Should().BeFalse("this code should not get executed");
+            return Result.Success(string.Join(" ", firstName, lastName, email, anotherEmail));
+        });
+
+        actual.IsFailure.Should().BeTrue();
+        actual.Errors.Should().HaveCount(2);
+        actual.Errors.Should().BeEquivalentTo(new ErrorList(
+            Error.Validation("LastName", "LastName cannot be empty"),
+            Error.Validation("Email", "Email address is not valid")));
+    }
+
 }
