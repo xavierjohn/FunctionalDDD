17fc6c6573de47d6a4ebd3cb9dbeaab24476a036
Xavier <xavierjohn@hotmail.com>
2022-12-09 12:27:32 -0800

    Generate Bind variations using T4
    Generate Bind variations using T4
    Combine several .cs files.

---
+++ Core/src/Functional/Result/Extensions/BindTs.cs
@@ -0,0 +1,129 @@
+﻿
+// Generated code
+namespace FunctionalDDD.Core;
+
+public static partial class ResultExtensions
+{
+
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2,TResult>(
+        this Result<(T1, T2)> result
+        , Func<T1, T2,Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (args1, args2) = result.Value;
+        return func(args1, args2);
+    }
+
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2, T3,TResult>(
+        this Result<(T1, T2, T3)> result
+        , Func<T1, T2, T3,Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (args1, args2, args3) = result.Value;
+        return func(args1, args2, args3);
+    }
+
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2, T3, T4,TResult>(
+        this Result<(T1, T2, T3, T4)> result
+        , Func<T1, T2, T3, T4,Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (args1, args2, args3, args4) = result.Value;
+        return func(args1, args2, args3, args4);
+    }
+
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2, T3, T4, T5,TResult>(
+        this Result<(T1, T2, T3, T4, T5)> result
+        , Func<T1, T2, T3, T4, T5,Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (args1, args2, args3, args4, args5) = result.Value;
+        return func(args1, args2, args3, args4, args5);
+    }
+
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2, T3, T4, T5, T6,TResult>(
+        this Result<(T1, T2, T3, T4, T5, T6)> result
+        , Func<T1, T2, T3, T4, T5, T6,Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (args1, args2, args3, args4, args5, args6) = result.Value;
+        return func(args1, args2, args3, args4, args5, args6);
+    }
+
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2, T3, T4, T5, T6, T7,TResult>(
+        this Result<(T1, T2, T3, T4, T5, T6, T7)> result
+        , Func<T1, T2, T3, T4, T5, T6, T7,Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (args1, args2, args3, args4, args5, args6, args7) = result.Value;
+        return func(args1, args2, args3, args4, args5, args6, args7);
+    }
+
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2, T3, T4, T5, T6, T7, T8,TResult>(
+        this Result<(T1, T2, T3, T4, T5, T6, T7, T8)> result
+        , Func<T1, T2, T3, T4, T5, T6, T7, T8,Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (args1, args2, args3, args4, args5, args6, args7, args8) = result.Value;
+        return func(args1, args2, args3, args4, args5, args6, args7, args8);
+    }
+
+
+    /// <summary>
+    ///     Selects result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static Result<TResult> Bind<T1, T2, T3, T4, T5, T6, T7, T8, T9,TResult>(
+        this Result<(T1, T2, T3, T4, T5, T6, T7, T8, T9)> result
+        , Func<T1, T2, T3, T4, T5, T6, T7, T8, T9,Result<TResult>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<TResult>(result.Errors);
+
+        var (args1, args2, args3, args4, args5, args6, args7, args8, args9) = result.Value;
+        return func(args1, args2, args3, args4, args5, args6, args7, args8, args9);
+    }
+
+
+}
--- Core/src/Functional/Result/Extensions/Map.cs
+++ Core/src/Functional/Result/Extensions/Map.cs
@@ -15,4 +15,49 @@ public static partial class ResultExtensions
         return Result.Success<K>(func(result.Value));
     }
 
+    /// <summary>
+    ///     Creates a new result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static async Task<Result<K>> Map<T, K, E>(this Task<Result<T>> resultTask, Func<T, Task<K>> func)
+    {
+        Result<T> result = await resultTask.DefaultAwait();
+
+        if (result.IsFailure)
+            return Result.Failure<K>(result.Error);
+
+        K value = await func(result.Value).DefaultAwait();
+
+        return Result.Success<K>(value);
+    }
+
+    /// <summary>
+    ///     Creates a new result from the return value of a given function. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static async Task<Result<K>> Map<T, K>(this Task<Result<T>> resultTask, Func<T, Task<K>> func)
+    {
+        Result<T> result = await resultTask.DefaultAwait();
+
+        if (result.IsFailure)
+            return Result.Failure<K>(result.Error);
+
+        K value = await func(result.Value).DefaultAwait();
+
+        return Result.Success(value);
+    }
+
+    /// <summary>
+    ///     Creates a new result from the return value of a given valueTask action. If the calling Result is a failure, a new failure result is returned instead.
+    /// </summary>
+    public static async ValueTask<Result<K>> Map<T, K>(this ValueTask<Result<T>> resultTask, Func<T, ValueTask<K>> valueTask)
+    {
+        Result<T> result = await resultTask;
+
+        if (result.IsFailure)
+            return Result.Failure<K>(result.Error);
+
+        K value = await valueTask(result.Value);
+
+        return Result.Success(value);
+    }
+
 }
