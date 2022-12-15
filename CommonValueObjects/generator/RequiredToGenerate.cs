8b2a4c57f24d0117acfa9d37225351f92fca182e
Xavier John <xavierjohn@hotmail.com>
2022-12-15 15:34:11 -0800

    Use IIncrementalGenerator

---
+++ CommonValueObjects/generator/RequiredToGenerate.cs
@@ -0,0 +1,17 @@
+﻿namespace FunctionalDDD.CommonValueObjectGenerator
+{
+
+    internal class RequiredToGenerate
+    {
+        public readonly string NameSpace;
+        public readonly string ClassName;
+        public readonly string ClassBase;
+
+        public RequiredToGenerate(string nameSpace, string className, string classBase)
+        {
+            NameSpace = nameSpace;
+            ClassName = className;
+            ClassBase = classBase;
+        }
+    }
+}
