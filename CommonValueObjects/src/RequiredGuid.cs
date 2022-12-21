676ff3bfec3bfc100535063ffa1b1bc0d2d14ae8
Xavier John <xavierjohn@hotmail.com>
2022-12-21 10:59:34 -0800

    DRY out Required.

--- CommonValueObjects/src/RequiredGuid.cs
+++ CommonValueObjects/src/RequiredGuid.cs
@@ -1,41 +1,21 @@
 ﻿namespace FunctionalDDD.CommonValueObjects;
-using System.Linq.Expressions;
-using System.Reflection;
-
-public abstract class RequiredGuid<T> : SimpleValueObject<Guid>
+public abstract class RequiredGuid<T> : Required<Guid, T>
     where T : RequiredGuid<T>
 {
-    private static readonly Lazy<Func<Guid, T>> CreateInstance = new Lazy<Func<Guid, T>>(CreateInstanceFunc);
-    private static readonly Error cannotBeEmptyError = Error.Validation($"{typeof(T).Name.ToCamelCase()}", $"{typeof(T).Name.SplitPascalCase()} cannot be empty");
 
     protected RequiredGuid(Guid value) : base(value)
     {
     }
 
 
-    public static Result<T> Create(Maybe<Guid> requiredStringOrNothing)
+    public static T CreateUnique() => CreateInstance.Value(Guid.NewGuid());
+
+    public static Result<T> Create(Maybe<Guid> requiredGuidOrNothing)
     {
-        return requiredStringOrNothing
-            .ToResult(cannotBeEmptyError)
-            .Ensure(x => x != Guid.Empty, cannotBeEmptyError)
+        return requiredGuidOrNothing
+            .ToResult(CannotBeEmptyError)
+            .Ensure(x => x != Guid.Empty, CannotBeEmptyError)
             .Map(guid => CreateInstance.Value(guid));
     }
 
-    public static T CreateUnique() => CreateInstance.Value(Guid.NewGuid());
-
-    private static Func<Guid, T> CreateInstanceFunc()
-    {
-        var flags = BindingFlags.NonPublic | BindingFlags.Instance;
-        var ctor = typeof(T).GetTypeInfo().GetConstructors(flags).Single(
-            ctors =>
-            {
-                var parameters = ctors.GetParameters();
-                return parameters.Length == 1 && parameters[0].ParameterType == typeof(Guid);
-            });
-        var value = Expression.Parameter(typeof(Guid), "value");
-        var body = Expression.New(ctor, value);
-        var lambda = Expression.Lambda<Func<Guid, T>>(body, value);
-
-        return lambda.Compile();
-    }
 }
