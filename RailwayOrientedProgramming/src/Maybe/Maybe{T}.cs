69c106468c1005fd9607b4bb02260013a0a266eb
Xavier John <xavierjohn@hotmail.com>
2023-01-02 19:29:48 -0800

    Alternate implementation.

--- RailwayOrientedProgramming/src/Maybe/Maybe{T}.cs
+++ RailwayOrientedProgramming/src/Maybe/Maybe{T}.cs
@@ -1,145 +1,79 @@
 ﻿namespace FunctionalDDD;
-using System.Runtime.CompilerServices;
 
-public readonly struct Maybe<T> : IEquatable<Maybe<T>>, IEquatable<object>, IMaybe<T>
+using System.Diagnostics.CodeAnalysis;
+
+public readonly struct Maybe<T> :
+    IEquatable<T>,
+    IEquatable<Maybe<T>>,
+    IMaybe<T>
 {
     private const string NoValue = "Maybe has no value.";
-    private readonly bool _isValueSet;
+    private readonly (bool IsSet, T? Value) inner;
 
-    private readonly T _value;
+    public T GetValueOrThrow(string? errorMessage = null) =>
+        inner.IsSet
+        ? inner.Value!
+        : throw new InvalidOperationException(errorMessage ?? NoValue);
 
-    /// <summary>
-    /// Returns the inner value if there's one, otherwise throws an InvalidOperationException with <paramref name="errorMessage"/>
-    /// </summary>
-    /// <exception cref="InvalidOperationException">Maybe has no value.</exception>
-    public T GetValueOrThrow(string? errorMessage = null)
+    public T GetValueOrDefault(T defaultValue) => inner.IsSet ? inner.Value! : defaultValue;
+
+    public bool TryGetValue([MaybeNullWhen(false)] out T value)
     {
-        if (HasNoValue)
-            throw new InvalidOperationException(errorMessage ?? NoValue);
-
-        return _value;
+        value = inner.Value;
+        return inner.IsSet;
     }
 
-    public T GetValueOrDefault(T defaultValue = default!)
-    {
-        if (HasNoValue)
-            return defaultValue;
-
-        return _value;
-    }
-
-    /// <summary>
-    ///  Indicates whether the inner value is present and returns the value if it is.
-    /// </summary>
-    /// <param name="value">The inner value, if present; otherwise `default`</param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool TryGetValue(
-        out T value)
-    {
-        value = _value;
-        return _isValueSet;
-    }
-
-    /// <summary>
-    /// Try to use GetValueOrThrow() or GetValueOrDefault() instead for better explicitness.
-    /// </summary>
     public T Value => GetValueOrThrow();
 
-    public bool HasValue => _isValueSet;
-    public bool HasNoValue => !HasValue;
+    public bool HasValue => inner.IsSet;
 
-    internal Maybe(T? value)
-    {
-        if (value == null)
-        {
-            _isValueSet = false;
-            _value = default!;
-            return;
-        }
+    public bool HasNoValue => !inner.IsSet;
 
-        _isValueSet = true;
-        _value = value;
-    }
+    internal Maybe(T? value) => inner = (value is not null, value);
 
-    public static implicit operator Maybe<T>(T? value)
-    {
-        if (value is Maybe<T> m)
-            return m;
+    public static implicit operator Maybe<T>(T? value) =>
+        value is Maybe<T> maybe ? maybe : new(value);
 
-        return Maybe.From(value);
-    }
+    public static bool operator ==(Maybe<T> maybe, T value) => maybe.Equals(value);
 
-    public static bool operator ==(Maybe<T> maybe, T value)
-    {
-        if (value is Maybe<T>)
-            return maybe.Equals(value);
-
-        if (maybe._value is null)
-            return value is null;
-
-        return maybe._value.Equals(value);
-    }
-
-    public static bool operator !=(Maybe<T> maybe, T value) => !(maybe == value);
+    public static bool operator !=(Maybe<T> maybe, T value) => !maybe.Equals(value);
 
     public static bool operator ==(Maybe<T> maybe, object other) => maybe.Equals(other);
 
-    public static bool operator !=(Maybe<T> maybe, object other) => !(maybe == other);
+    public static bool operator !=(Maybe<T> maybe, object other) => !maybe.Equals(other);
 
     public static bool operator ==(Maybe<T> first, Maybe<T> second) => first.Equals(second);
 
-    public static bool operator !=(Maybe<T> first, Maybe<T> second) => !(first == second);
+    public static bool operator !=(Maybe<T> first, Maybe<T> second) => !first.Equals(second);
 
-    public override bool Equals(object? obj)
-    {
-        if (obj is null)
-            return false;
-        if (obj is Maybe<T> other)
-            return Equals(other);
-        if (obj is T value)
-            return Equals(value);
-        return false;
-    }
+    public override bool Equals(object? obj) =>
+        obj switch
+        {
+            Maybe<T> other => Equals(other),
+            T other => Equals(other),
+            _ => false,
+        };
 
-    public bool Equals(Maybe<T> other)
-    {
-        if (HasNoValue && other.HasNoValue)
-            return true;
+    public bool Equals(Maybe<T> other) =>
+        inner.IsSet && other.inner.IsSet
+        ? EqualityComparer<T>.Default.Equals(inner.Value!, other.inner.Value!)
+        : !inner.IsSet && !other.inner.IsSet;
 
-        if (HasNoValue || other.HasNoValue)
-            return false;
+    public bool Equals(T? other) =>
+        inner.IsSet
+        ? EqualityComparer<T>.Default.Equals(inner.Value, other)
+        : !inner.IsSet;
 
-        return EqualityComparer<T>.Default.Equals(_value, other._value);
-    }
+    public override int GetHashCode() => inner.Value?.GetHashCode() ?? 0;
 
-    public override int GetHashCode()
-    {
-        if (HasNoValue)
-            return 0;
-
-        return _value?.GetHashCode() ?? 0;
-    }
-
-    public override string ToString()
-    {
-        if (_value is null)
-            return NoValue;
-
-        return _value?.ToString() ?? NoValue;
-    }
+    public override string ToString() => inner.Value?.ToString() ?? NoValue;
 }
 
-/// <summary>
-/// Non-generic entry point for <see cref="Maybe{T}" /> members
-/// </summary>
 public readonly struct Maybe
 {
-    public static Maybe<T> None<T>() => new Maybe<T>();
+    public static Maybe<T> None<T>() => new();
 
-    /// <summary>
-    /// Creates a new <see cref="Maybe{T}" /> from the provided <paramref name="value"/>
-    /// </summary>
-    public static Maybe<T> From<T>(T? value) => new Maybe<T>(value);
+    public static Maybe<T> From<T>(T? value) => new(value);
 }
 
 /// <summary>
