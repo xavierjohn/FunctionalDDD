703e927bb26e74e65b20dc39268e221e9faca67d
Xavier John <xavierjohn@hotmail.com>
2023-01-20 19:15:22 -0800

    Sample

--- Examples/Xunit/AsyncUsageExamples.cs
+++ Examples/Xunit/AsyncUsageExamples.cs
@@ -1,27 +1,46 @@
 ﻿
 namespace Example.Tests;
 using FunctionalDDD;
+using Xunit;
 
 public class AsyncUsageExamples
 {
-    public static async Task<string> Promote_with_async_methods_in_the_beginning_of_the_chain(long id) =>
-        await GetByIdAsync(id)
+    [Fact]
+    public static async Task Promote_with_async_methods_in_the_beginning_of_the_chain()
+    {
+        var id = 1;
+
+        var result = await GetByIdAsync(id)
             .ToResultAsync(Error.NotFound("Customer with such Id is not found: " + id))
             .EnsureAsync(customer => customer.CanBePromoted, Error.Validation("The customer has the highest status possible"))
             .TapAsync(customer => customer.Promote())
             .BindAsync(customer => EmailGateway.SendPromotionNotification(customer.Email))
             .FinallyAsync(result => result.IsSuccess ? "Okay" : result.Error.Message);
 
-    public static async Task<string> Promote_with_async_methods_in_the_beginning_and_in_the_middle_of_the_chain(long id) =>
-        await GetByIdAsync(id)
+        result.Should().Be("Okay");
+    }
+
+    [Fact]
+    public static async Task Promote_with_async_methods_in_the_beginning_and_in_the_middle_of_the_chain()
+    {
+        var id = 1;
+
+        var result = await GetByIdAsync(id)
             .ToResultAsync(Error.NotFound("Customer with such Id is not found: " + id))
             .EnsureAsync(customer => customer.CanBePromoted, Error.Validation("The customer has the highest status possible"))
             .TapAsync(customer => customer.PromoteAsync())
             .BindAsync(customer => EmailGateway.SendPromotionNotificationAsync(customer.Email))
             .FinallyAsync(result => result.IsSuccess ? "Okay" : result.Error.Message);
 
-    public static async Task<string> Promote_with_async_methods_in_the_beginning_and_in_the_middle_of_the_chain_using_compensate(long id) =>
-        await GetByIdAsync(id)
+        result.Should().Be("Okay");
+    }
+
+    [Fact]
+    public static async Task Promote_with_async_methods_in_the_beginning_and_in_the_middle_of_the_chain_using_compensate()
+    {
+        var id = 1;
+
+        var result = await GetByIdAsync(id)
             .ToResultAsync(Error.NotFound("Customer with such Id is not found: " + id))
             .EnsureAsync(customer => customer.CanBePromoted, Error.Validation("Need to ask manager"))
             .TapErrorAsync(error => Log(error))
@@ -30,6 +49,9 @@ public class AsyncUsageExamples
             .TapAsync(customer => customer.PromoteAsync())
             .BindAsync(customer => EmailGateway.SendPromotionNotificationAsync(customer.Email))
             .FinallyAsync(result => result.IsSuccess ? "Okay" : result.Error.Message);
+
+        result.Should().Be("Okay");
+    }
 
     static void Log(ErrorList message)
     {
