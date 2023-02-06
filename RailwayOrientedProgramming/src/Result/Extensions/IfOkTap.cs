15d5536686b03053474a31f7f692eae724b6e7a6
Xavier John <xavierjohn@hotmail.com>
2023-02-06 11:51:41 -0800

    IfOkTap

--- RailwayOrientedProgramming/src/Result/Extensions/IfOkTap.cs
+++ RailwayOrientedProgramming/src/Result/Extensions/IfOkTap.cs
@@ -5,7 +5,7 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Executes the given action if the calling result is a success. Returns the calling result.
     /// </summary>
-    public static Result<TOk, Error> IfOk<TOk>(this Result<TOk, Error> result, Action action)
+    public static Result<TOk, Error> IfOkTap<TOk>(this Result<TOk, Error> result, Action action)
     {
         if (result.IsOk)
             action();
@@ -16,7 +16,7 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Executes the given action if the calling result is a success. Returns the calling result.
     /// </summary>
-    public static Result<TOk, Error> IfOk<TOk>(this Result<TOk, Error> result, Action<TOk> action)
+    public static Result<TOk, Error> IfOkTap<TOk>(this Result<TOk, Error> result, Action<TOk> action)
     {
         if (result.IsOk)
             action(result.Ok);
@@ -27,9 +27,9 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Executes the given action if the calling result is a success. Returns the calling result.
     /// </summary>
-    public static async Task<Result<TOk, Error>> IfOkAsync<TOk>(this Task<Result<TOk, Error>> resultTask, Action<TOk> action)
+    public static async Task<Result<TOk, Error>> IfOkTapAsync<TOk>(this Task<Result<TOk, Error>> resultTask, Action<TOk> action)
     {
         Result<TOk, Error> result = await resultTask.ConfigureAwait(false);
-        return result.IfOk(action);
+        return result.IfOkTap(action);
     }
 }
