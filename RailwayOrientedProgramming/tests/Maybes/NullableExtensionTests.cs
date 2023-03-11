8c05a73bb4e97584821402eabd42b5f8554d3c90
Xavier John <xavierjohn@hotmail.com>
2023-03-10 16:48:06 -0800

    Add more tests.

--- RailwayOrientedProgramming/tests/Maybes/NullableExtensionTests.cs
+++ RailwayOrientedProgramming/tests/Maybes/NullableExtensionTests.cs
@@ -61,6 +61,7 @@ public class NullableExtensionTests
         result.Error.Should().Be(Error.Validation("MyClass is not set."));
     }
 
+    // async class
     [Fact]
     public async Task Convert_task_nullable_class_to_result_pass()
     {
@@ -120,6 +121,68 @@ public class NullableExtensionTests
         // Assert
         result.IsFailure.Should().BeTrue();
         result.Error.Should().Be(Error.Validation("MyClass is not set."));
+    }
+
+    // async struct
+    [Fact]
+    public async Task Convert_task_nullable_struct_to_result_pass()
+    {
+        // Arrange
+        DateTime? my = DateTime.Now;
+        var myClassTask = Task.FromResult(my);
+
+        // Act
+        var result = await myClassTask.ToResultAsync(Error.Validation("Date is not set."));
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(my);
+    }
+
+    [Fact]
+    public async Task Convert_task_nullable_struct_to_result_fail()
+    {
+        // Arrange
+        DateTime? my = default;
+        var myClassTask = Task.FromResult(my);
+
+        // Act
+        var result = await myClassTask.ToResultAsync(Error.Validation("Date is not set."));
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(Error.Validation("Date is not set."));
+    }
+
+    [Fact]
+    public async Task Convert_valuetask_nullable_struct_to_result_pass()
+    {
+        // Arrange
+        DateTime? my = DateTime.Now;
+
+        var myClassTask = ValueTask.FromResult(my);
+
+        // Act
+        var result = await myClassTask.ToResultAsync(Error.Validation("Date is not set."));
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(my);
+    }
+
+    [Fact]
+    public async Task Convert_valuetask_nullable_struct_to_result_fail()
+    {
+        // Arrange
+        MyClass? my = null;
+        var myClassTask = ValueTask.FromResult(my);
+
+        // Act
+        var result = await myClassTask.ToResultAsync(Error.Validation("Date is not set."));
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(Error.Validation("Date is not set."));
     }
 }
 
