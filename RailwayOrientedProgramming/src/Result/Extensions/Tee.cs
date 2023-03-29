2b0b5f5524f92b9a08df939e80a6c9f214866aca
Xavier <xavierjohn@hotmail.com>
2023-03-29 12:24:27 -0700

    Add Tee extension methods.

--- RailwayOrientedProgramming/src/Result/Extensions/Tee.cs
+++ RailwayOrientedProgramming/src/Result/Extensions/Tee.cs
@@ -27,6 +27,15 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Executes the given action if the calling result is a success. Returns the calling result.
     /// </summary>
+    public static async Task<Result<TOk, Error>> TeeAsync<TOk>(this Task<Result<TOk, Error>> resultTask, Action action)
+    {
+        Result<TOk, Error> result = await resultTask.ConfigureAwait(false);
+        return result.Tee(action);
+    }
+
+    /// <summary>
+    ///     Executes the given action if the calling result is a success. Returns the calling result.
+    /// </summary>
     public static async Task<Result<TOk, Error>> TeeAsync<TOk>(this Task<Result<TOk, Error>> resultTask, Action<TOk> action)
     {
         Result<TOk, Error> result = await resultTask.ConfigureAwait(false);
@@ -36,7 +45,7 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Executes the given action if the calling result is a success. Returns the calling result.
     /// </summary>
-    public static async Task<Result<TOk, TError>> TeeAsync<TOk, TError>(this Result<TOk, TError> result, Func<Task> func)
+    public static async Task<Result<TOk, Error>> TeeAsync<TOk>(this Result<TOk, Error> result, Func<Task> func)
     {
         if (result.IsSuccess)
             await func().ConfigureAwait(false);
@@ -47,7 +56,32 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Executes the given action if the calling result is a success. Returns the calling result.
     /// </summary>
-    public static async Task<Result<TOk, TError>> TeeAsync<TOk, TError>(this Result<TOk, TError> result, Func<TOk, Task> func)
+    public static async Task<Result<TOk, Error>> TeeAsync<TOk>(this Task<Result<TOk, Error>> resultTask, Func<Task> func)
+    {
+        Result<TOk, Error> result = await resultTask.ConfigureAwait(false);
+
+        if (result.IsSuccess)
+            await func().ConfigureAwait(false);
+
+        return result;
+    }
+
+    /// <summary>
+    ///     Executes the given action if the calling result is a success. Returns the calling result.
+    /// </summary>
+    public static async Task<Result<TOk, Error>> TeeAsync<TOk>(this Task<Result<TOk, Error>> resultTask, Func<TOk, Task> func)
+    {
+        Result<TOk, Error> result = await resultTask.ConfigureAwait(false);
+        if (result.IsSuccess)
+            await func(result.Value).ConfigureAwait(false);
+
+        return result;
+    }
+
+    /// <summary>
+    ///     Executes the given action if the calling result is a success. Returns the calling result.
+    /// </summary>
+    public static async Task<Result<TOk, Error>> TeeAsync<TOk>(this Result<TOk, Error> result, Func<TOk, Task> func)
     {
         if (result.IsSuccess)
             await func(result.Value).ConfigureAwait(false);
@@ -58,7 +92,7 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Executes the given action if the calling result is a success. Returns the calling result.
     /// </summary>
-    public static async ValueTask<Result<TOk, TError>> TeeAsync<TOk, TError>(this Result<TOk, TError> result, Func<ValueTask> func)
+    public static async ValueTask<Result<TOk, Error>> TeeAsync<TOk>(this Result<TOk, Error> result, Func<ValueTask> func)
     {
         if (result.IsSuccess)
             await func().ConfigureAwait(false);
@@ -69,11 +103,55 @@ public static partial class ResultExtensions
     /// <summary>
     ///     Executes the given action if the calling result is a success. Returns the calling result.
     /// </summary>
-    public static async ValueTask<Result<TOk, TError>> TeeAsync<TOk, TError>(this Result<TOk, TError> result, Func<TOk, ValueTask> func)
+    public static async ValueTask<Result<TOk, Error>> TeeAsync<TOk>(this Result<TOk, Error> result, Func<TOk, ValueTask> func)
     {
         if (result.IsSuccess)
             await func(result.Value).ConfigureAwait(false);
 
         return result;
+    }
+
+    /// <summary>
+    ///     Executes the given action if the calling result is a success. Returns the calling result.
+    /// </summary>
+    public static async ValueTask<Result<TOk, Error>> TeeAsync<TOk>(this ValueTask<Result<TOk, Error>> resultTask, Func<ValueTask> valueTask)
+    {
+        Result<TOk, Error> result = await resultTask;
+
+        if (result.IsSuccess)
+            await valueTask();
+
+        return result;
+    }
+
+    /// <summary>
+    ///     Executes the given action if the calling result is a success. Returns the calling result.
+    /// </summary>
+    public static async ValueTask<Result<TOk, Error>> TeeAsync<TOk>(this ValueTask<Result<TOk, Error>> resultTask, Func<TOk, ValueTask> valueTask)
+    {
+        Result<TOk, Error> result = await resultTask;
+
+        if (result.IsSuccess)
+            await valueTask(result.Value);
+
+        return result;
+    }
+
+    /// <summary>
+    ///     Executes the given action if the calling result is a success. Returns the calling result.
+    /// </summary>
+    public static async ValueTask<Result<TOk, Error>> TeeAsync<TOk>(this ValueTask<Result<TOk, Error>> resultTask, Action action)
+    {
+        Result<TOk, Error> result = await resultTask;
+        return result.Tee(action);
+    }
+
+    /// <summary>
+    ///     Executes the given action if the calling result is a success. Returns the calling result.
+    /// </summary>
+    public static async ValueTask<Result<TOk, Error>> TeeAsync<TOk>(this ValueTask<Result<TOk, Error>> resultTask, Action<TOk> action)
+    {
+        Result<TOk, Error> result = await resultTask;
+        return result.Tee(action);
     }
 }
