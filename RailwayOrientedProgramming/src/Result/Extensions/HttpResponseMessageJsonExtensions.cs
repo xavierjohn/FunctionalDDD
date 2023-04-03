61b658f60b8097c0ad8a3f524d6f2070303a532e
Xavier <xavierjohn@hotmail.com>
2023-04-03 11:56:36 -0700

    Support for Task ReadResultWithNotFoundAsync. (#67)

--- RailwayOrientedProgramming/src/Result/Extensions/HttpResponseMessageJsonExtensions.cs
+++ RailwayOrientedProgramming/src/Result/Extensions/HttpResponseMessageJsonExtensions.cs
@@ -25,4 +25,26 @@ public static class HttpResponseMessageJsonExtensions
         var t = await response.Content.ReadFromJsonAsync<T>(jsonSerializerOptions, cancellationToken).ConfigureAwait(false);
         return (t is null) ? Result.Failure<T>(notFoundError) : Result.Success(t);
     }
+
+    public static Task<Result<T>> ReadResultWithNotFoundAsync<T>(
+    this Task<HttpResponseMessage> responseTask,
+    NotFoundError notFoundError,
+    CancellationToken cancellationToken = default)
+        => responseTask.ReadResultWithNotFoundAsync<T>(notFoundError, null, cancellationToken);
+
+    public static async Task<Result<T>> ReadResultWithNotFoundAsync<T>(
+    this Task<HttpResponseMessage> responseTask,
+    NotFoundError notFoundError,
+    JsonSerializerOptions? jsonSerializerOptions,
+    CancellationToken cancellationToken = default)
+    {
+        using var response = await responseTask.ConfigureAwait(false);
+        if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            return Result.Failure<T>(notFoundError);
+
+        response.EnsureSuccessStatusCode();
+
+        var t = await response.Content.ReadFromJsonAsync<T>(jsonSerializerOptions, cancellationToken).ConfigureAwait(false);
+        return (t is null) ? Result.Failure<T>(notFoundError) : Result.Success(t);
+    }
 }
