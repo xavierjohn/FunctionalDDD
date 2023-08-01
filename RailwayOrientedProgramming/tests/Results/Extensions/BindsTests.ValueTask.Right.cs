350e6b57bdc1a95db4401e047afea289746e85e3
Xavier <xavierjohn@hotmail.com>
2023-08-01 12:46:41 -0700

    HttpPartialObjectResultTest

--- RailwayOrientedProgramming/tests/Results/Extensions/BindsTests.ValueTask.Right.cs
+++ RailwayOrientedProgramming/tests/Results/Extensions/BindsTests.ValueTask.Right.cs
@@ -6,7 +6,7 @@
     public class BindTests_ValueTask_Right : BindBase
     {
         [Fact]
-        public async ValueTask Bind_ValueTask_Right_T_K_returns_failure_and_does_not_execute_func()
+        public async Task Bind_ValueTask_Right_T_K_returns_failure_and_does_not_execute_func()
         {
             Result<K> output = await Failure_T().BindAsync(Func_T_ValueTask_Success_K);
 
@@ -14,7 +14,7 @@
         }
 
         [Fact]
-        public async ValueTask Bind_ValueTask_Right_T_K_selects_new_result()
+        public async Task Bind_ValueTask_Right_T_K_selects_new_result()
         {
             Result<K> output = await Success_T(T.Value).BindAsync(Func_T_ValueTask_Success_K);
 
