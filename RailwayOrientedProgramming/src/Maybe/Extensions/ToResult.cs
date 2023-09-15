645c6c04a15ca0cd5eb264242c8ae968ae94850a
Xavier <xavierjohn@hotmail.com>
2023-09-15 15:23:49 -0700

    Maybe documentation.

--- RailwayOrientedProgramming/src/Maybe/Extensions/ToResult.cs
+++ RailwayOrientedProgramming/src/Maybe/Extensions/ToResult.cs
@@ -4,49 +4,100 @@ using FunctionalDDD.RailwayOrientedProgramming.Errors;
 
 public static partial class MaybeExtensions
 {
-    public static Result<TOk> ToResult<TOk>(in this Maybe<TOk> maybe, Error error)
-        where TOk : notnull
+    /// <summary>
+    /// If <see cref="Maybe{TValue}"/> has a value, return a <see cref="Result{TValue}"/><br/>
+    /// Otherwise, return the <see cref="Error"/>
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value contained in Maybe.</typeparam>
+    /// <param name="maybe"></param>
+    /// <param name="error">One of the classes derived from <see cref="Error"/></param>
+    /// <returns></returns>
+    public static Result<TValue> ToResult<TValue>(in this Maybe<TValue> maybe, Error error)
+        where TValue : notnull
     {
         if (maybe.HasNoValue)
-            return Result.Failure<TOk>(error);
+            return Result.Failure<TValue>(error);
 
         return Result.Success(maybe.GetValueOrThrow());
     }
 
-    public static async Task<Result<TOk>> ToResultAsync<TOk>(this Task<Maybe<TOk>> maybeTask, Error error)
-        where TOk : notnull
+    /// <summary>
+    /// If <see cref="Maybe{TValue}"/> has a value, return a <see cref="Result{TValue}"/><br/>
+    /// Otherwise, return the <see cref="Error"/>
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value contained in Maybe.</typeparam>
+    /// <param name="maybe"></param>
+    /// <param name="error">One of the classes derived from <see cref="Error"/></param>
+    /// <returns></returns>
+    public static Result<TValue> ToResult<TValue>(in this Maybe<TValue> maybe, Func<Error> ferror)
+    where TValue : notnull
+    {
+        if (maybe.HasNoValue)
+            return Result.Failure<TValue>(ferror);
+
+        return Result.Success(maybe.GetValueOrThrow());
+    }
+}
+
+public static partial class MaybeExtensionsAsync
+{
+    /// <summary>
+    /// If <see cref="Maybe{TValue}"/> has a value, return a <see cref="Result{TValue}"/><br/>
+    /// Otherwise, return the <see cref="Error"/>
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value contained in Maybe.</typeparam>
+    /// <param name="maybe"></param>
+    /// <param name="error">One of the classes derived from <see cref="Error"/></param>
+    /// <returns></returns>
+    public static async Task<Result<TValue>> ToResultAsync<TValue>(this Task<Maybe<TValue>> maybeTask, Error error)
+        where TValue : notnull
     {
         var maybe = await maybeTask.ConfigureAwait(false);
         return maybe.ToResult(error);
     }
 
-    public static async ValueTask<Result<TOk>> ToResultAsync<TOk>(this ValueTask<Maybe<TOk>> maybeTask, Error error)
-        where TOk : notnull
+    /// <summary>
+    /// If <see cref="Maybe{TValue}"/> has a value, return a <see cref="Result{TValue}"/><br/>
+    /// Otherwise, return the <see cref="Error"/>
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value contained in Maybe.</typeparam>
+    /// <param name="maybe"></param>
+    /// <param name="error">One of the classes derived from <see cref="Error"/></param>
+    /// <returns></returns>
+    public static async ValueTask<Result<TValue>> ToResultAsync<TValue>(this ValueTask<Maybe<TValue>> maybeTask, Error error)
+        where TValue : notnull
     {
-        Maybe<TOk> maybe = await maybeTask;
+        Maybe<TValue> maybe = await maybeTask;
         return maybe.ToResult(error);
     }
 
-    public static Result<TOk> ToResult<TOk>(in this Maybe<TOk> maybe, Func<Error> ferror)
-    where TOk : notnull
-    {
-        if (maybe.HasNoValue)
-            return Result.Failure<TOk>(ferror);
-
-        return Result.Success(maybe.GetValueOrThrow());
-    }
-
-    public static async Task<Result<TOk>> ToResultAsync<TOk>(this Task<Maybe<TOk>> maybeTask, Func<Error> ferror)
-        where TOk : notnull
+    /// <summary>
+    /// If <see cref="Maybe{TValue}"/> has a value, return a <see cref="Result{TValue}"/><br/>
+    /// Otherwise, return the <see cref="Error"/>
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value contained in Maybe.</typeparam>
+    /// <param name="maybe"></param>
+    /// <param name="error">One of the classes derived from <see cref="Error"/></param>
+    /// <returns></returns>
+    public static async Task<Result<TValue>> ToResultAsync<TValue>(this Task<Maybe<TValue>> maybeTask, Func<Error> ferror)
+        where TValue : notnull
     {
         var maybe = await maybeTask.ConfigureAwait(false);
         return maybe.ToResult(ferror);
     }
 
-    public static async ValueTask<Result<TOk>> ToResultAsync<TOk>(this ValueTask<Maybe<TOk>> maybeTask, Func<Error> ferror)
-        where TOk : notnull
+    /// <summary>
+    /// If <see cref="Maybe{TValue}"/> has a value, return a <see cref="Result{TValue}"/><br/>
+    /// Otherwise, return the <see cref="Error"/>
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value contained in Maybe.</typeparam>
+    /// <param name="maybe"></param>
+    /// <param name="error">One of the classes derived from <see cref="Error"/></param>
+    /// <returns></returns>
+    public static async ValueTask<Result<TValue>> ToResultAsync<TValue>(this ValueTask<Maybe<TValue>> maybeTask, Func<Error> ferror)
+        where TValue : notnull
     {
-        Maybe<TOk> maybe = await maybeTask;
+        Maybe<TValue> maybe = await maybeTask;
         return maybe.ToResult(ferror);
     }
 }
