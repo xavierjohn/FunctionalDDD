9c7ca3b339c0dd2eb774d98d2d5ca4fc733e8a32
Xavier <xavierjohn@hotmail.com>
2023-09-20 10:15:05 -0700

    Move FluentValidation to FluentValidation namespace.

--- FluentValidation/src/FluentValidationExtension.cs
+++ FluentValidation/src/FluentValidationExtension.cs
@@ -1,11 +1,52 @@
-﻿namespace FunctionalDDD.Results;
+﻿namespace FluentValidation;
 
 using System.Linq;
+using FluentValidation.Results;
+using FunctionalDDD.Results;
 using FunctionalDDD.Results.Errors;
 
-public static class FluentValidationExtension
+/// <summary>
+/// Converts a <see cref="ValidationResult"/> to a <see cref="Result{T}"/>.
+/// </summary>
+/// <example>
+/// Validation of an user object using FluentValidation:
+/// <code>
+/// class User : AggregateRoot&lt;UserId&gt;
+///{
+///    public FirstName FirstName { get; }
+///    public LastName LastName { get; }
+///    public EmailAddress Email { get; }
+///    public string Password { get; }
+///
+///    public static Result&lt;User&gt; New(FirstName firstName, LastName lastName, EmailAddress email, string password)
+///    {
+///        var user = new User(firstName, lastName, email, password);
+///        return s_validator.ValidateToResult(user);
+///    }
+///
+///
+///    private User(FirstName firstName, LastName lastName, EmailAddress email, string password)
+///        : base(UserId.NewUnique())
+///    {
+///        FirstName = firstName;
+///        LastName = lastName;
+///        Email = email;
+///        Password = password;
+///    }
+///
+///    static readonly InlineValidator&lt;User&gt; s_validator = new()
+///    {
+///        v =&gt; v.RuleFor(x =&gt; x.FirstName).NotNull(),
+///        v =&gt; v.RuleFor(x =&gt; x.LastName).NotNull(),
+///        v =&gt; v.RuleFor(x =&gt; x.Email).NotNull(),
+///        v =&gt; v.RuleFor(x =&gt; x.Password).NotEmpty()
+///    };
+///}
+/// </code>
+/// </example>
+public static class FunctionalDDDValidationExtension
 {
-    public static Result<T> ToResult<T>(this global::FluentValidation.Results.ValidationResult validationResult, T value)
+    public static Result<T> ToResult<T>(this ValidationResult validationResult, T value)
     {
         if (validationResult.IsValid)
             return Result.Success<T>(value);
@@ -17,6 +58,6 @@ public static class FluentValidationExtension
         return Result.Failure<T>(Error.Validation(errors));
     }
 
-    public static Result<T> ValidateToResult<T>(this global::FluentValidation.IValidator<T> validator, T value) =>
+    public static Result<T> ValidateToResult<T>(this IValidator<T> validator, T value) =>
         validator.Validate(value).ToResult(value);
 }
