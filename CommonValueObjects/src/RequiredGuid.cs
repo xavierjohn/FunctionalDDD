a483937ee9ba695052005d6698636b71be0f0b7a
Xavier <xavierjohn@hotmail.com>
2023-09-20 16:45:51 -0700

    Update

--- CommonValueObjects/src/RequiredGuid.cs
+++ CommonValueObjects/src/RequiredGuid.cs
@@ -7,7 +7,7 @@
 /// <example>
 /// This example shows how to create a strongly named Value Object MenuId that cannot have default Guid value.
 /// <code>
-/// partial class MenuId : RequiredGuid&lt;FirstName&gt;
+/// partial class MenuId : RequiredGuid&lt;MenuId&gt;
 /// </code>
 /// **Note** The partial keyword is required to allow the code generator to add the generated methods.
 /// </example>
