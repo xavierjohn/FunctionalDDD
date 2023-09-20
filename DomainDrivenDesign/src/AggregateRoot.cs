ca4bef876436b8a68f1e65be6692eac2ea235b80
Xavier <xavierjohn@hotmail.com>
2023-09-19 17:48:51 -0700

    Update doc.

--- DomainDrivenDesign/src/AggregateRoot.cs
+++ DomainDrivenDesign/src/AggregateRoot.cs
@@ -1,9 +1,10 @@
 ﻿namespace FunctionalDDD.Domain;
+
 /// <summary>
 /// A DDD aggregate is a cluster of domain objects that can be treated as a single unit. 
 /// An aggregate will have one of its component objects be the aggregate root.Any references from outside the aggregate should only go to the aggregate root.
 /// The root can thus ensure the integrity of the aggregate as a whole.
-
+/// <typeparam name="TId"></typeparam>
 public abstract class AggregateRoot<TId> : Aggregate<TId>
     where TId : notnull
 {
