f0be7b819c681553cf5d04c09edcfa1f2130b318
Xavier <xavierjohn@hotmail.com>
2023-10-19 10:05:15 -0700

    Optional parameter

---
+++ RailwayOrientedProgramming/src/Optional.cs
@@ -0,0 +1,14 @@
+﻿namespace FunctionalDDD.Results;
+using System;
+
+public static class OptionalExtenstions
+{
+    public static Result<Maybe<TOut>> Optional<TIn, TOut>(TIn? value, Func<TIn, Result<TOut>> function)
+        where TOut : notnull
+    {
+        if (value is null)
+            return Maybe.None<TOut>();
+
+        return function(value).Map(r => Maybe.From(r));
+    }
+}
---
+++ RailwayOrientedProgramming/tests/OptionalTests.cs
@@ -0,0 +1,63 @@
+﻿namespace RailwayOrientedProgramming.Tests;
+using FunctionalDDD.Results;
+using FunctionalDDD.Results.Errors;
+
+public class OptionalTests
+{
+    [Fact]
+    public void Will_return_Maybe_Value()
+    {
+        // Arrange
+        string? zipCode = "92874";
+
+        // Act
+        var result = OptionalExtenstions.Optional(zipCode, ZipCode.New);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Value.Zip.Should().Be(zipCode);
+    }
+
+    [Fact]
+    public void Will_return_Maybe_None()
+    {
+        // Arrange
+        string? zipCode = null;
+
+        // Act
+        var result = OptionalExtenstions.Optional(zipCode, ZipCode.New);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.HasNoValue.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Will_return_Failure()
+    {
+        // Arrange
+        string? zipCode = "Hi";
+
+        // Act
+        var result = OptionalExtenstions.Optional(zipCode, ZipCode.New);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().BeOfType<BadRequestError>();
+    }
+
+    class ZipCode
+    {
+        public string Zip { get; }
+
+        private ZipCode(string zipCode) => Zip = zipCode;
+
+        public static Result<ZipCode> New(string? zipCode)
+        {
+            if (string.IsNullOrEmpty(zipCode)) return Result.Failure<ZipCode>(Error.BadRequest("ZipCode is required."));
+            if (zipCode.Length != 5) return Result.Failure<ZipCode>(Error.BadRequest("Invalid ZipCode."));
+
+            return new ZipCode(zipCode);
+        }
+    }
+}
