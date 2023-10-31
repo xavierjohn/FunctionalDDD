62b04e8c836c71a0717dceb905518587364aee3d
Xavier <xavierjohn@hotmail.com>
2023-10-30 18:15:19 -0700

    Benchmark (#104)
    Simple benchmark comparing ROP style code vs If statements.

---
+++ Benchmark/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Benchmark;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<BenchmarkROP>();
+
+var benchmark = new BenchmarkROP();
+var happyRop = benchmark.RopStyleHappy();
+var happyIf = benchmark.IfStyleHappy();
+if (happyRop != happyIf)
+    throw new InvalidDataException("Happy: Somethings wrong with the world today.");
+
+var sadRop = benchmark.RopStyleSad();
+var sadIf = benchmark.IfStyleSad();
+if (sadRop != sadIf)
+    throw new InvalidDataException("Sad: Somethings wrong with the world today.");
