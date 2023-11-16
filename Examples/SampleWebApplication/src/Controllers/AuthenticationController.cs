6307a0e439fde3cf705aff64f6d7155f766af132
Xavier <xavierjohn@hotmail.com>
2023-11-15 16:45:26 -0800

    duplicate controller error.

--- Examples/SampleWebApplication/src/Controllers/AuthenticationController.cs
+++ Examples/SampleWebApplication/src/Controllers/AuthenticationController.cs
@@ -4,6 +4,7 @@ using FunctionalDdd;
 using Microsoft.AspNetCore.Mvc;
 using SampleWebApplication.Model;
 
+[ApiController]
 [Route("[controller]")]
 public class AuthenticationController : ControllerBase
 {
@@ -15,35 +16,35 @@ public class AuthenticationController : ControllerBase
         .Bind((firstName, lastName, email) => SampleWebApplication.User.TryCreate(firstName, lastName, email, request.password))
         .ToOkActionResult(this);
 
-    [HttpPost("[action]")]
-    public ActionResult<User> RegisterCreated([FromBody] RegisterUserRequest request) =>
-        FirstName.TryCreate(request.firstName)
-        .Combine(LastName.TryCreate(request.lastName))
-        .Combine(EmailAddress.TryCreate(request.email))
-        .Bind((firstName, lastName, email) => SampleWebApplication.User.TryCreate(firstName, lastName, email, request.password))
-        .Finally(result => result.IsSuccess
-            ? CreatedAtAction("Get", new { name = result.Value.FirstName }, result.Value)
-            : result.ToErrorActionResult(this));
+    //[HttpPost("[action]")]
+    //public ActionResult<User> RegisterCreated([FromBody] RegisterUserRequest request) =>
+    //    FirstName.TryCreate(request.firstName)
+    //    .Combine(LastName.TryCreate(request.lastName))
+    //    .Combine(EmailAddress.TryCreate(request.email))
+    //    .Bind((firstName, lastName, email) => SampleWebApplication.User.TryCreate(firstName, lastName, email, request.password))
+    //    .Finally(result => result.IsSuccess
+    //        ? CreatedAtAction("Get", new { name = result.Value.FirstName }, result.Value)
+    //        : result.ToErrorActionResult(this));
 
-    [HttpPost("[action]")]
-    public ActionResult<User> RegisterCreated2([FromBody] RegisterUserRequest request) =>
-        FirstName.TryCreate(request.firstName)
-        .Combine(LastName.TryCreate(request.lastName))
-        .Combine(EmailAddress.TryCreate(request.email))
-        .Bind((firstName, lastName, email) => SampleWebApplication.User.TryCreate(firstName, lastName, email, request.password))
-        .Finally(
-            ok => CreatedAtAction("Get", new { name = ok.FirstName }, ok),
-            err => err.ToErrorActionResult<User>(this));
+    //[HttpPost("[action]")]
+    //public ActionResult<User> RegisterCreated2([FromBody] RegisterUserRequest request) =>
+    //    FirstName.TryCreate(request.firstName)
+    //    .Combine(LastName.TryCreate(request.lastName))
+    //    .Combine(EmailAddress.TryCreate(request.email))
+    //    .Bind((firstName, lastName, email) => SampleWebApplication.User.TryCreate(firstName, lastName, email, request.password))
+    //    .Finally(
+    //        ok => CreatedAtAction("Get", new { name = ok.FirstName }, ok),
+    //        err => err.ToErrorActionResult<User>(this));
 
-    [HttpPost("[action]")]
-    public ActionResult<User> RegisterAccepted([FromBody] RegisterUserRequest request) =>
-        FirstName.TryCreate(request.firstName)
-        .Combine(LastName.TryCreate(request.lastName))
-        .Combine(EmailAddress.TryCreate(request.email))
-        .Bind((firstName, lastName, email) => SampleWebApplication.User.TryCreate(firstName, lastName, email, request.password))
-        .Finally(result => result.IsSuccess
-            ? AcceptedAtAction("Get", new { name = result.Value.FirstName }, result.Value)
-            : result.ToErrorActionResult(this));
+    //[HttpPost("[action]")]
+    //public ActionResult<User> RegisterAccepted([FromBody] RegisterUserRequest request) =>
+    //    FirstName.TryCreate(request.firstName)
+    //    .Combine(LastName.TryCreate(request.lastName))
+    //    .Combine(EmailAddress.TryCreate(request.email))
+    //    .Bind((firstName, lastName, email) => SampleWebApplication.User.TryCreate(firstName, lastName, email, request.password))
+    //    .Finally(result => result.IsSuccess
+    //        ? AcceptedAtAction("Get", new { name = result.Value.FirstName }, result.Value)
+    //        : result.ToErrorActionResult(this));
 
     [HttpGet("[action]")]
     public ActionResult<string> Get(string name) => Ok($"Hello {name}!");
