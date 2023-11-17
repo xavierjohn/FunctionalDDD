95edeb6c750ef383bf9b0c4f88ce84a448db7e02
Xavier <xavierjohn@hotmail.com>
2023-11-17 09:17:52 -0800

    Support IParsable for RequiredString, RequiredGuid and EmailAddresses. (#114)
    * IParsable for RequiredString and RequiredGuid
    
    * IParsable for EmailAddress.

--- CommonValueObjects/tests/EmailAddressTests.cs
+++ CommonValueObjects/tests/EmailAddressTests.cs
@@ -1,20 +1,12 @@
 ﻿namespace CommonValueObjects.Tests;
 
-
 public class EmailAddressTests
 {
     [Theory]
-    [InlineData("xavier")]
-    [InlineData("xavier@")]
-    [InlineData("")]
-    [InlineData(null)]
-    [InlineData("@com")]
-    [InlineData("@#@@##@%^%#$@#$@#.com")]
-    [InlineData("John Doe <example@email.com>")]
-    [InlineData("CAT…123@email.com")]
+    [MemberData(nameof(GetBadEmailAddresses))]
     public void Cannot_create_invalid_email(string email)
     {
-        // Arrange
+        // Arrange & Act
         var result = EmailAddress.TryCreate(email, "school email");
 
         // Assert
@@ -23,9 +15,7 @@ public class EmailAddressTests
     }
 
     [Theory]
-    [InlineData("xavier@somewhere.com")]
-    [InlineData("0987654321@example.com")]
-    [InlineData("_______@email.com")]
+    [MemberData(nameof(GetGoodEmailAddresses))]
     public void Can_create_valid_email(string email)
     {
         // Arrange
@@ -35,4 +25,72 @@ public class EmailAddressTests
         result.IsSuccess.Should().BeTrue();
         result.Value.Should().BeOfType<EmailAddress>();
     }
+
+    [Theory]
+    [MemberData(nameof(GetGoodEmailAddresses))]
+    public void Can_create_EmailAddress_try_parsing_valid_string(string strEmail)
+    {
+        // Arrange & Act
+        EmailAddress.TryParse(strEmail, null, out var email)
+
+        // Assert
+        .Should().BeTrue();
+        email.Should().BeOfType<EmailAddress>();
+        email!.ToString().Should().Be(strEmail);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetBadEmailAddresses))]
+    public void Cannot_create_EmailAddress_try_parsing_invalid_string(string strEmail)
+    {
+        // Arrange & Act
+        EmailAddress.TryParse(strEmail, null, out var email)
+
+        // Assert
+        .Should().BeFalse();
+        email.Should().BeNull();
+    }
+
+    [Theory]
+    [MemberData(nameof(GetGoodEmailAddresses))]
+    public void Can_create_EmailAddress_parsing_valid_string(string strEmail)
+    {
+        // Arrange & Act
+        var email = EmailAddress.Parse(strEmail, null);
+
+        // Assert
+        email.Should().BeOfType<EmailAddress>();
+        email.ToString().Should().Be(strEmail);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetBadEmailAddresses))]
+    public void Cannot_create_EmailAddress_parsing_invalid_string(string email)
+    {
+        // Arrange & Act
+        Action act = () => EmailAddress.Parse(email, null);
+
+        // Assert
+        act.Should().Throw<FormatException>()
+            .WithMessage("Email address is not valid.");
+    }
+
+    public static TheoryData<string?> GetGoodEmailAddresses() => new()
+    {
+        "xavier@somewhere.com",
+        "0987654321@example.com",
+        "_______@email.com"
+    };
+
+    public static TheoryData<string?> GetBadEmailAddresses() => new()
+    {
+        null,
+        string.Empty,
+        "xavier",
+        "xavier@",
+        "@com",
+        "@#@@##@%^%#$@#$@#.com",
+        "John Doe <example@email.com>",
+        "CAT…123@email.com"
+    };
 }
