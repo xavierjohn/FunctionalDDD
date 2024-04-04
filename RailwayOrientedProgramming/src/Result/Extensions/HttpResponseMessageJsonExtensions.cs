c3c76b5e2fc9b88642ac45b029c759711f36a08e
Xavier <xavierjohn@hotmail.com>
2024-04-04 10:41:58 -0700

    Rename to ResultReadValueAsync that will throw if the HTTP content is null

--- RailwayOrientedProgramming/src/Result/Extensions/HttpResponseMessageJsonExtensions.cs
+++ RailwayOrientedProgramming/src/Result/Extensions/HttpResponseMessageJsonExtensions.cs
@@ -1,39 +1,72 @@
 ﻿namespace FunctionalDdd;
 
+using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 
 public static class HttpResponseMessageJsonExtensionsAsync
 {
-    public static async Task<Result<T>> ReadResultWithNotFoundAsync<T>(
+    /// <summary>
+    /// Make the http request and return a Result with the deserialized value.
+    /// If the http response status code is 404, return a Result with the notFoundError.
+    /// If the response content is null, throw a JsonException.
+    /// </summary>
+    /// <typeparam name="T">The type for the <see cref="Result{T}"></see> object.</typeparam>
+    /// <param name="response">Http response.</param>
+    /// <param name="notFoundError">The <see cref="NotFoundError"></see> to return if the http response status code is <see cref="HttpStatusCode.NotFound"></see> </param>
+    /// <param name="jsonTypeInfo">Provides JSON serialization-related metadata about a type.</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>A <see cref="Result{T}"/> object with the value.</returns>
+    public static async Task<Result<T>> ResultReadValueAsync<T>(
         this HttpResponseMessage response,
         NotFoundError notFoundError,
         JsonTypeInfo<T> jsonTypeInfo,
         CancellationToken cancellationToken = default)
     {
-        if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+        if (response.StatusCode == HttpStatusCode.NotFound)
             return Result.Failure<T>(notFoundError);
 
         response.EnsureSuccessStatusCode();
 
-        var t = await response.Content.ReadFromJsonAsync<T>(jsonTypeInfo, cancellationToken)
-            .ConfigureAwait(false);
-        return (t is null) ? Result.Failure<T>(notFoundError) : Result.Success(t);
+        return await ReadAndThrowIfResponseIsNull(response, jsonTypeInfo, cancellationToken);
     }
 
-    public static async Task<Result<T>> ReadResultWithNotFoundAsync<T>(
+    /// <summary>
+    /// Make the http request and return a Result with the deserialized value.
+    /// If the http response status code is 404, return a Result with the notFoundError.
+    /// If the response content is null, throw a JsonException.
+    /// </summary>
+    /// <typeparam name="T">The type for the <see cref="Result{T}"></see> object.</typeparam>
+    /// <param name="responseTask">A <see cref="Task"></see> that returns a <see cref="HttpResponseMessage"></see></param>
+    /// <param name="notFoundError">The <see cref="NotFoundError"></see> to return if the http response status code is <see cref="HttpStatusCode.NotFound"></see> </param>
+    /// <param name="jsonTypeInfo">Provides JSON serialization-related metadata about a type.</param>
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>A <see cref="Result{T}"/> object with the value.</returns>>
+    public static async Task<Result<T>> ResultReadValueAsync<T>(
         this Task<HttpResponseMessage> responseTask,
         NotFoundError notFoundError,
         JsonTypeInfo<T> jsonTypeInfo,
         CancellationToken cancellationToken = default)
     {
-        using var response = await responseTask.ConfigureAwait(false);
-        return await response.ReadResultWithNotFoundAsync<T>(notFoundError, jsonTypeInfo, cancellationToken)
-            .ConfigureAwait(false);
+        using var response = await responseTask;
+        return await response.ResultReadValueAsync(notFoundError, jsonTypeInfo, cancellationToken);
     }
 
-    public static async Task<Result<TValue>> ReadResultAsync<TValue, TContext>(
+    /// <summary>
+    /// Make the http request and return a Result with the deserialized value
+    /// If the http response status code is not a success status code, return a Result with the error from the callbackFailedStatusCode.
+    /// If the response content is null, throw a JsonException.
+    /// </summary>
+    /// <typeparam name="TValue">The type for the <see cref="Result{T}"></see> object.</typeparam>
+    /// <typeparam name="TContext">The Http Context.</typeparam>
+    /// <param name="response">Http response.</param>
+    /// <param name="callbackFailedStatusCode">The function to call to get the error object if the response is in a failed state.</param>
+    /// <param name="context">HTTP context that is passed to the callback function.</param>
+    /// <param name="jsonTypeInfo">Provides JSON serialization-related metadata about a type.</param
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>A <see cref="Result{T}"/> object with the value.</returns>>
+    public static async Task<Result<TValue>> ResultReadValueAsync<TValue, TContext>(
         this HttpResponseMessage response,
         Func<HttpResponseMessage, TContext, Task<Error>> callbackFailedStatusCode,
         TContext context,
@@ -46,20 +79,35 @@ public static class HttpResponseMessageJsonExtensionsAsync
             return Result.Failure<TValue>(error);
         }
 
-        var t = await response.Content.ReadFromJsonAsync<TValue>(jsonTypeInfo, cancellationToken)
-            .ConfigureAwait(false);
-        return (t is null) ? throw new JsonException($"Failed to create {typeof(TValue).Name} from Json") : Result.Success(t);
+        return await ReadAndThrowIfResponseIsNull(response, jsonTypeInfo, cancellationToken);
     }
 
-    public static async Task<Result<TValue>> ReadResultAsync<TValue, TContext>(
+    /// <summary>
+    /// Make the http request and return a Result with the deserialized value
+    /// If the http response status code is not a success status code, return a Result with the error from the callbackFailedStatusCode.
+    /// If the response content is null, throw a JsonException.
+    /// </summary>
+    /// <typeparam name="TValue">The type for the <see cref="Result{T}"></see> object.</typeparam>
+    /// <typeparam name="TContext">The Http Context.</typeparam>
+    /// <param name="responseTask">A <see cref="Task"></see> that returns a <see cref="HttpResponseMessage"></see></param>
+    /// <param name="callbackFailedStatusCode">The function to call to get the error object if the response is in a failed state.</param>
+    /// <param name="context">HTTP context that is passed to the callback function.</param>
+    /// <param name="jsonTypeInfo">Provides JSON serialization-related metadata about a type.</param
+    /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+    /// <returns>A <see cref="Result{T}"/> object with the value.</returns>>
+    public static async Task<Result<TValue>> ResultReadValueAsync<TValue, TContext>(
         this Task<HttpResponseMessage> responseTask,
         Func<HttpResponseMessage, TContext, Task<Error>> callbackFailedStatusCode,
         TContext context,
         JsonTypeInfo<TValue> jsonTypeInfo,
         CancellationToken cancellationToken = default)
     {
-        using var response = await responseTask.ConfigureAwait(false);
-        return await response.ReadResultAsync<TValue, TContext>(callbackFailedStatusCode, context, jsonTypeInfo, cancellationToken)
-            .ConfigureAwait(false);
+        using var response = await responseTask;
+        return await response.ResultReadValueAsync(callbackFailedStatusCode, context, jsonTypeInfo, cancellationToken);
+    }
+    private static async Task<Result<TValue>> ReadAndThrowIfResponseIsNull<TValue>(HttpResponseMessage response, JsonTypeInfo<TValue> jsonTypeInfo, CancellationToken cancellationToken)
+    {
+        var t = await response.Content.ReadFromJsonAsync(jsonTypeInfo, cancellationToken);
+        return (t is null) ? throw new JsonException($"Failed to create {typeof(TValue).Name} from Json") : Result.Success(t);
     }
 }
