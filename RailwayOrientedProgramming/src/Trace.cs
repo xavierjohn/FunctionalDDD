599983fee3ddb04904701f43a76a91b634ff0a81
Xavier <xavierjohn@hotmail.com>
2024-04-12 12:17:31 -0700

    Update activity source

--- RailwayOrientedProgramming/src/Trace.cs
+++ RailwayOrientedProgramming/src/Trace.cs
@@ -3,10 +3,10 @@
 using System.Diagnostics;
 using System.Reflection;
 
-internal class Trace
+internal static class Trace
 {
     internal static readonly AssemblyName AssemblyName = typeof(Trace).Assembly.GetName();
-    internal static readonly string ActivitySourceName = AssemblyName.Name!;
+    internal static readonly string ActivitySourceName = "Functional DDD";
     internal static readonly Version Version = AssemblyName.Version!;
     internal static readonly ActivitySource ActivitySource = new(ActivitySourceName, Version.ToString());
 }
