2c615a283aa8498777ab1f74f9cb5d2c973ccf9c
Xavier <xavierjohn@hotmail.com>
2024-04-20 10:26:02 -0700

    ParallalAsync and AwaitAsync tests. (#155)
    ParallalAsync and AwaitAsync tests.

--- RailwayOrientedProgramming/src/Result/Extensions/ParallelAsync.cs
+++ RailwayOrientedProgramming/src/Result/Extensions/ParallelAsync.cs
@@ -15,5 +15,4 @@ public static partial class ParallelExtensionsAsync
     /// <returns></returns>
     public static (Task<Result<T1>>, Task<Result<T2>>) ParallelAsync<T1, T2>(this Task<Result<T1>> resultTask1, Task<Result<T2>> resultTask2)
         => (resultTask1, resultTask2);
-
 }
