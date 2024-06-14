cbb9309fcff7f1d1d2a0c12f1d4a5edf5b6a575f
Xavier <xavierjohn@hotmail.com>
2024-06-14 12:03:23 -0700

    Use file scope

--- CommonValueObjects/generator/RequiredPartialClassInfo.cs
+++ CommonValueObjects/generator/RequiredPartialClassInfo.cs
@@ -1,19 +1,18 @@
-﻿namespace FunctionalDdd.CommonValueObjectGenerator
+﻿namespace FunctionalDdd.CommonValueObjectGenerator;
+
+internal class RequiredPartialClassInfo
 {
-    internal class RequiredPartialClassInfo
+    public readonly string NameSpace;
+    public readonly string ClassName;
+    public readonly string ClassBase;
+    public readonly string Accessibility;
+
+    public RequiredPartialClassInfo(string nameSpace, string className, string classBase, string accessibility)
     {
-        public readonly string NameSpace;
-        public readonly string ClassName;
-        public readonly string ClassBase;
-        public readonly string Accessibility;
-
-        public RequiredPartialClassInfo(string nameSpace, string className, string classBase, string accessibility)
-        {
-            NameSpace = nameSpace;
-            ClassName = className;
-            ClassBase = classBase;
-            Accessibility = accessibility;
-        }
-
+        NameSpace = nameSpace;
+        ClassName = className;
+        ClassBase = classBase;
+        Accessibility = accessibility;
     }
+
 }
