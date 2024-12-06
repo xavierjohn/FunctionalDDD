23b8aa40c25354115e11688b4e9601ad390fdaf1
Xavier <xavierjohn@hotmail.com>
2024-12-06 11:26:17 -0800

    Add back null check.

--- Examples/SampleUserLibrary/Aggregate/User.cs
+++ Examples/SampleUserLibrary/Aggregate/User.cs
@@ -29,13 +29,19 @@ public class User : Aggregate<UserId>
 
     public class UserValidator : AbstractValidator<User>
     {
-        public UserValidator() => RuleFor(user => user.Password)
+        public UserValidator()
+        {
+            RuleFor(user => user.FirstName).NotNull();
+            RuleFor(user => user.LastName).NotNull();
+            RuleFor(user => user.Email).NotNull();
+            RuleFor(user => user.Password)
                 .NotEmpty().WithMessage("Password must not be empty.")
                 .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
                 .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
                 .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
                 .Matches("[0-9]").WithMessage("Password must contain at least one number.")
                 .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+        }
     }
 
 }
